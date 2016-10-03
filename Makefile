3d0fd5bf0e3fd5c7fa1d4bf2fdb73e3c04a82316
googyanas <googyanas@gmail.com>
2016-10-03 16:22:43 +0000

    buildenv

--- Makefile
+++ Makefile
@@ -412,8 +412,8 @@ KBUILD_CFLAGS   := -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs \
 		   -fno-strict-aliasing -fno-common \
 		   -Werror-implicit-function-declaration \
 		   -Wno-format-security -Wno-logical-not-parentheses \
-		   -mtune=cortex-a72.cortex-a53 \
-		   -std=gnu89
+		   -mtune=exynos-m1 \
+		   -std=gnu89 -funsafe-math-optimizations
 
 KBUILD_AFLAGS_KERNEL :=
 KBUILD_CFLAGS_KERNEL :=
--- build_kernel_s7.sh
+++ build_kernel_s7.sh
@@ -5,7 +5,7 @@ export KCONFIG_NOTIMESTAMP=true
 export ARCH=arm64
 export SUBARCH=arm64
 
-VER="\"-GoogyMax-S7-MM-v$1\""
+VER="\"-GoogyMax-S7_MM_v$1\""
 cp -f /home/anas/S7/Kernel/arch/arm64/configs/googymax-s7_defconfig /home/anas/S7/googymax-s7_defconfig
 sed "s#^CONFIG_LOCALVERSION=.*#CONFIG_LOCALVERSION=$VER#" /home/anas/S7/googymax-s7_defconfig > /home/anas/S7/Kernel/arch/arm64/configs/googymax-s7_defconfig
 
@@ -28,9 +28,9 @@ cd /home/anas/S7/Out
 ./packimg.sh
 
 cd /home/anas/S7/Release
-zip -r ../GoogyMax-S7_Kernel_${1}.zip .
+zip -r ../GoogyMax-S7_Kernel_MM_${1}.zip .
 
-adb push /home/anas/S7/GoogyMax-S7_Kernel_${1}.zip /sdcard/GoogyMax-S7_Kernel_${1}.zip
+adb push /home/anas/S7/GoogyMax-S7_Kernel_MM_${1}.zip /sdcard/GoogyMax-S7_Kernel_MM_${1}.zip
 
 adb kill-server
 
